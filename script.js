27f5ec160b681b38221365d29098af1db89938da
Rafael_MMIX <183986062+RafaelMMIX@users.noreply.github.com>
2025-07-03 17:12:54 +0200

    Update script.js
    prout

--- script.js
+++ script.js
@@ -22,7 +22,9 @@
 const buttonEl = document.querySelector("button[name='generate']");
 const buttonCopy = document.querySelector("button[name='copy']");
 const PasswordEl = document.querySelector("input[name='password']");
-const longueurTxt = document.querySelector
+const longueurRangeEl = document.querySelector("input[name='pwdLengthSlider']");
+const longueurInputEl = document.querySelector("input[name='pwdLengthIn']");
+const errorMsgEl = document.querySelector("#error-msg");
 const majList = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 // const minList = "abcdefghijklmnopqrstuvwxyz";
 const chiffreList = "0123456789";
@@ -35,12 +37,11 @@ function getRandomArbitrary(min, max) {
   return Math.random() * (max - min) + min;
 }
 
-
 function generate() {
   const maj = document.querySelector("input[name='maj']").checked;
   const chiffre = document.querySelector("input[name='chiffres']").checked;
   const special = document.querySelector("input[name='speciaux']").checked;
-  let newPwd = ""
+  let newPwd = "";
   let availableDict = "abcdefghijklmnopqrstuvwxyz";
   if (maj) {
     availableDict = availableDict + majList;
@@ -53,20 +54,48 @@ function generate() {
   }
   console.log(availableDict);
 
-
-  const longueur = document.querySelector("input[name='longueur']").value;
+  const longueur = document.querySelector(
+    "input[name='pwdLengthSlider']"
+  ).value;
   console.log(maj, chiffre, special, longueur);
 
   for (let pas = 0; pas < longueur; pas++) {
     // Ceci sera exécuté 5 fois
     // À chaque éxécution, la variable "pas" augmentera de 1
     // Lorsque'elle sera arrivée à 5, le boucle se terminera.
-    newPwd = newPwd + availableDict[getRandomArbitrary(0, availableDict.length - 1) | 0];
+    newPwd =
+      newPwd +
+      availableDict[getRandomArbitrary(0, availableDict.length - 1) | 0];
     PasswordEl.value = newPwd;
   }
 }
 
 buttonCopy.addEventListener("click", () => {
-navigator.clipboard.writeText(document.querySelector("input[name='password']").value); 
+  navigator.clipboard.writeText(
+    document.querySelector("input[name='password']").value
+  );
 });
 buttonEl.addEventListener("click", generate);
+
+longueurRangeEl.addEventListener("change", onSliderChange);
+longueurInputEl.addEventListener("change", onNumberChange);
+
+function onSliderChange(event) {
+  longueurInputEl.value = event.target.value;
+}
+
+function onNumberChange(event) {
+  longueurRangeEl.value = event.target.value;
+  isRangeValid(longueurInputEl.value);
+}
+
+function isRangeValid(value) {
+  if (value >= 4 && value <= 30) {
+    errorMsgEl.innerText = "";
+  } else {
+    errorMsgEl.innerText = "La longueur doit être comprise entre 4 et 30 😠";
+  }
+}
+
+// value <= 4 X
+// value >= 30 X
